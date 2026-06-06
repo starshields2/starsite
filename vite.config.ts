9659139d2abbc12162b1b1d3b9d0a47e64b964f1
Sam <istarshieldsi@gmail.com>
2026-06-06 13:40:46 -0500

    Revert "Update vite.config.ts"
    This reverts commit 9016fb0d63d57550d99a4cd6e26669280fb77231.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/starshieldsportfolio/',
+  base: '/portfolio-01/',
   plugins: [
     react({
       babel: {
